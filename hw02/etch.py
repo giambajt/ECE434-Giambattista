b0495144614f73c8bddfbb410d6e9a97e8ed8b92
giambajt <giambajt@rose-hulman.edu>
2021-09-15 20:27:37 -0400

    added etch.py to hw02 directory

---
+++ hw02/etch.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+#check if the command line was run with 2 arguments, otherwise default the etch window size
+if len(sys.argv) == 3:
+    etchx = int(sys.argv[1])
+    etchy = int(sys.argv[2])
+else:
+    etchx=600
+    etchy=400
+#general setup for pygame and some user variables
+screen = pygame.display.set_mode((600,400))
+x= etchx//2
+y= etchy//2
+opening_screen=1#binary value that dictates whether the computer is on the opening screen or the etch screen
+black= (0,0,0)
+white = (255,255,255)
+#setup the basics for pygame and all of the text instructions on the opening screen
+clock = pygame.time.Clock()
+screen.fill(white)
+pygame.display.set_caption('etch_a_sktech')
+font = pygame.font.Font('freesansbold.ttf',16) 
+text = font.render('Welcome to Etch-A-Sketch!', True, black, white)
+textRect = text.get_rect()
+textRect.center = (300, 200)
+instructionText = font.render('Instructions: Move the line using the arrow keys and', True, black, white)
+insTextRect = instructionText.get_rect()
+insTextRect.center = (300, 200+20)
+instructionText2 = font.render('shake the pad with space bar. Press escape to exit.', True, black, white)
+insTextRect2 = instructionText2.get_rect()
+insTextRect2.center = (300, 200+40)
+instructionText3 = font.render('Press the space bar to start the game', True, black, white)
+insTextRect3 = instructionText3.get_rect()
+insTextRect3.center = (300, 200+60)
+
+
+#main loop
+while 1:
+    #makes game run at 60 FPS and updates the screen
+    clock.tick(60)
+    pygame.display.update()
+    #etch screen
+    if opening_screen == 0:
+        #etch window event loop that draws the sketches and reads for keyboard inputs
+        pygame.draw.circle(screen, black, (x,y),2)
+        key = pygame.key.get_pressed()
+        if key[pygame.K_RIGHT]:x+=1
+        if key[pygame.K_LEFT]:x-=1
+        if key[pygame.K_UP]:y-=1
+        if key[pygame.K_DOWN]:y+=1
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                sys.exit()
+            
+            elif event.type == KEYDOWN and event.key == K_SPACE:
+                screen.fill(white)#screen 'shake'
+    #opening screen
+    elif opening_screen == 1:
+        #display all of the instruction texts
+        screen.blit(text,textRect)
+        screen.blit(instructionText, insTextRect)
+        screen.blit(instructionText2, insTextRect2)
+        screen.blit(instructionText3, insTextRect3)
+        #event loop that checks for keyboard presses
+        for event in pygame.event.get():
+            #When space is pressed, setup the screen for the etch window
+            if event.type == KEYDOWN and event.key == K_SPACE:
+                screen = pygame.display.set_mode((etchx,etchy))
+                screen.fill(white)
+                opening_screen = 0
+            elif event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                sys.exit()
